6093ad14cf6f5e85d544a53a460a543728904e13
mux99 <mux99@live.be>
2020-07-24 22:53:35 +0200

    Add UnityFiles
    Oupsie

---
+++ Unity/Assets/Scipts/menus/TimedSkip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimedSkip : MonoBehaviour
+{	
+	public float SkipTime = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SkipTime -= Time.deltaTime;
+
+        if (SkipTime <= 0)
+        {
+        	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
